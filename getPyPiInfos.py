45a2460086dfa5c5318da0c7064cf4ffb67a2946
thomaswoehlke <thomas.woehlke@gmail.com>
2021-09-14 10:56:19 +0200

    setup

--- getPyPiInfos.py
+++ getPyPiInfos.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
 
 packages = []
 
 with open('requirements.in') as f:
     for line in f.readlines():
-        print(line)
+        package = line.splitlines()[0].split('#')[0].split('[')[0]
+        if len(package) > 0:
+            packages.append(package)
 
 if not f.closed:
     f.close()
+
+print("")
+for package in packages:
+    out = '* [' + package + '](https://pypi.org/project/' + package + '/)'
+    print(out)
+print("")
